d074b09185704002c2de0165e2bb903f273aaba8
Dan Baston <dbaston@isciences.com>
2019-03-06 12:20:17 -0500

    Remove Dockerfile for GEOS 3.6 image

--- ci/docker_build_ci.sh
+++ ci/docker_build_ci.sh
@@ -3,6 +3,3 @@ set -e
 
 docker build -t isciences/exactextract-build-env:latest - < Dockerfile.gitlabci
 docker push isciences/exactextract-build-env:latest
-
-docker build -t isciences/exactextract-build-env:geos36 - < Dockerfile.geos36.gitlabci
-docker push isciences/exactextract-build-env:geos36
