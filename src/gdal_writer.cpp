c55c9c92b8c1b34fdaa8d8a101b108d375c13d19
Dan Baston <dbaston@isciences.com>
2019-03-06 12:16:30 -0500

    Tidy GDALWriter

--- src/gdal_writer.cpp
+++ src/gdal_writer.cpp
@@ -1,5 +1,102 @@
+// Copyright (c) 2019 ISciences, LLC.
+// All rights reserved.
 //
-// Created by dbaston on 3/6/19.
+// This software is licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License. You may
+// obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "gdal_writer.h"
+#include "stats_registry.h"
+#include "utils.h"
+
+#include "gdal.h"
+#include "ogr_api.h"
+
+namespace exactextract {
+    GDALWriter::GDALWriter(const std::string & filename, const std::string & id_field) :
+            m_layer{nullptr},
+            m_dataset{nullptr}
+    {
+        auto driver_name = get_driver_name(filename);
+        auto driver = GDALGetDriverByName(driver_name.c_str());
+
+        if (driver == nullptr) {
+            throw std::runtime_error("Could not load output driver: " + driver_name);
+        }
+
+        m_dataset = GDALCreate(driver, filename.c_str(), 0, 0, 0, GDT_Unknown, nullptr);
+        m_layer = GDALDatasetCreateLayer(m_dataset, "output", nullptr, wkbNone, nullptr);
+
+        auto id_def = OGR_Fld_Create(id_field.c_str(), OFTString);
+        OGR_Fld_SetWidth(id_def, 32); // FIXME
+        OGR_L_CreateField(m_layer, id_def, true);
+    }
+
+    GDALWriter::~GDALWriter() {
+        if (m_dataset != nullptr) {
+            GDALClose(m_dataset);
+        }
+    }
+
+    void GDALWriter::add_operation(const Operation & op) {
+        auto field_name = varname(op);
+
+        // TODO set type here?
+        auto def = OGR_Fld_Create(field_name.c_str(), OFTReal);
+        OGR_L_CreateField(m_layer, def, true);
+
+        m_ops.push_back(&op);
+    }
+
+    void GDALWriter::set_registry(const StatsRegistry* reg) {
+        m_reg = reg;
+    }
+
+    void GDALWriter::write(const std::string & fid) {
+        auto feature = OGR_F_Create(OGR_L_GetLayerDefn(m_layer));
+
+        OGR_F_SetFieldString(feature, 0, fid.c_str());
+
+        for (const auto &op : m_ops) {
+            if (m_reg->contains(fid, *op)) {
+                const auto field_pos = OGR_F_GetFieldIndex(feature, varname(*op).c_str());
+                const auto &stats = m_reg->stats(fid, *op);
+
+                if (op->stat == "mean") {
+                    OGR_F_SetFieldDouble(feature, field_pos, stats.mean());
+                } else if (op->stat == "sum") {
+                    OGR_F_SetFieldDouble(feature, field_pos, stats.sum());
+                } else {
+                    // FIXME support all stats
+                    OGR_F_Destroy(feature);
+                    throw std::runtime_error("Not implemented.");
+                }
+            }
+        }
+
+        // FIXME check return vals
+        OGR_L_CreateFeature(m_layer, feature);
+        OGR_F_Destroy(feature);
+    }
+
+    std::string GDALWriter::get_driver_name(const std::string & filename) {
+        if (ends_with(filename, ".csv")) {
+            return "CSV";
+        } else if (ends_with(filename, ".dbf")) {
+            return "ESRI Shapefile";
+        } else if (ends_with(filename, ".nc")) {
+            return "NetCDF";
+        } else if (starts_with(filename, "PG:")) {
+            return "PostgreSQL";
+        } else {
+            throw std::runtime_error("Unknown output format: " + filename);
+        }
+    }
+
+}
--- src/gdal_writer.h
+++ src/gdal_writer.h
@@ -1,107 +1,42 @@
+// Copyright (c) 2019 ISciences, LLC.
+// All rights reserved.
 //
-// Created by dbaston on 3/6/19.
+// This software is licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License. You may
+// obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef EXACTEXTRACT_GDAL_WRITER_H
 #define EXACTEXTRACT_GDAL_WRITER_H
 
 #include "output_writer.h"
 
-#include "gdal.h"
-#include "ogr_api.h"
-
-#include "stats_registry.h"
-
-#include "utils.h"
-
-
 namespace exactextract {
 
     class GDALWriter : public OutputWriter {
 
     public:
-        GDALWriter(const std::string & filename, const std::string & id_field) :
-        m_layer{nullptr},
-        m_dataset{nullptr} {
-            std::string driver_name;
+        GDALWriter(const std::string & filename, const std::string & id_field);
 
-            if (ends_with(filename, ".csv")) {
-                driver_name = "CSV";
-            } else if (ends_with(filename, ".dbf")) {
-                driver_name = "ESRI Shapefile";
-            } else if (ends_with(filename, ".nc")) {
-                driver_name = "NetCDF";
-            } else if (starts_with(filename, "PG:")) {
-                driver_name = "PostgreSQL";
-            } else {
-                throw std::runtime_error("Unknown output format: " + filename);
-            }
+        ~GDALWriter();
 
-            auto driver = GDALGetDriverByName(driver_name.c_str());
+        static std::string get_driver_name(const std::string & filename);
 
-            if (driver == nullptr) {
-                throw std::runtime_error("Could not load output driver: " + driver_name);
-            }
+        void add_operation(const Operation & op) override;
 
-            m_dataset = GDALCreate(driver, filename.c_str(), 0, 0, 0, GDT_Unknown, nullptr);
+        void set_registry(const StatsRegistry* reg) override;
 
-            m_layer = GDALDatasetCreateLayer(m_dataset, "output", nullptr, wkbNone, nullptr);
-
-            auto id_def = OGR_Fld_Create(id_field.c_str(), OFTString);
-
-            OGR_Fld_SetWidth(id_def, 32); // FIXME
-
-            OGR_L_CreateField(m_layer, id_def, true);
-        }
-
-        ~GDALWriter() {
-            if (m_dataset != nullptr) {
-                GDALClose(m_dataset);
-            }
-        }
-
-        void add_operation(const Operation & op) override {
-            auto field_name = varname(op);
-
-            // TODO set type here?
-            auto def = OGR_Fld_Create(field_name.c_str(), OFTReal);
-            OGR_L_CreateField(m_layer, def, true);
-
-            m_ops.push_back(&op);
-        }
-
-        void set_registry(const StatsRegistry* reg) override {
-            m_reg = reg;
-        }
-
-        void write(const std::string & fid) override {
-            auto feature = OGR_F_Create(OGR_L_GetLayerDefn(m_layer));
-
-            OGR_F_SetFieldString(feature, 0, fid.c_str());
-
-            for (const auto &op : m_ops) {
-                if (m_reg->contains(fid, *op)) {
-                    const auto field_pos = OGR_F_GetFieldIndex(feature, varname(*op).c_str());
-                    const auto &stats = m_reg->stats(fid, *op);
-
-                    if (op->stat == "mean") {
-                        OGR_F_SetFieldDouble(feature, field_pos, stats.mean());
-                    } else if (op->stat == "sum") {
-                        OGR_F_SetFieldDouble(feature, field_pos, stats.sum());
-                    } else {
-                        // FIXME
-                        OGR_F_Destroy(feature);
-                        throw std::runtime_error("Not implemented.");
-                    }
-                }
-            }
-
-            // FIXME check return vals
-            OGR_L_CreateFeature(m_layer, feature);
-            OGR_F_Destroy(feature);
-        }
+        void write(const std::string & fid) override;
 
     private:
+        using GDALDatasetH = void*;
+        using OGRLayerH = void*;
+
         GDALDatasetH m_dataset;
         OGRLayerH m_layer;
         const StatsRegistry* m_reg;
