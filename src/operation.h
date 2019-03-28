d01e105f3bd51c272cdb212905a79345617e0e1d
Dan Baston <dbaston@isciences.com>
2019-03-28 14:33:18 -0400

    Expose count stat in CLI

--- src/operation.h
+++ src/operation.h
@@ -47,6 +47,8 @@ namespace exactextract {
                 return [](const RasterStats<double> & s) { return s.mean(); };
             } else if (stat == "sum") {
                 return [](const RasterStats<double> & s) { return s.sum(); };
+            } else if (stat == "count") {
+                return [](const RasterStats<double> & s) { return s.count(); };
             } else if (stat == "weighted_mean") {
                 return [](const RasterStats<double> & s) { return s.weighted_mean(); };
             } else if (stat == "weighted_sum") {
