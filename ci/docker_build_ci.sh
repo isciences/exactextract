27314829fb9602896e2407952fdb6fec6a4c52ce
Daniel Baston <dbaston@gmail.com>
2019-04-02 20:59:49 -0400

    Streamline coverage build

--- ci/docker_build_ci.sh
+++ ci/docker_build_ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-docker build -t isciences/exactextract-build-env:latest - < Dockerfile.gitlabci
+docker build --pull -t isciences/exactextract-build-env:latest - < Dockerfile.gitlabci
 docker push isciences/exactextract-build-env:latest
