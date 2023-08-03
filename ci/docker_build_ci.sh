6fd3f4edc718564e95d467a1f3273a3163d18122
Daniel Baston <dbaston@gmail.com>
2023-08-03 18:54:58 -0400

    CI: Update GEOS versions

--- ci/docker_build_ci.sh
+++ ci/docker_build_ci.sh
@@ -5,20 +5,26 @@ set -euxo pipefail
 docker build --pull --build-arg GEOS_VERSION=3.5.2 --build-arg BUILD_TOOL=autotools -t isciences/exactextract-test-env:geos35 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos35
 
-docker build --pull --build-arg GEOS_VERSION=3.6.5 --build-arg BUILD_TOOL=autotools -t isciences/exactextract-test-env:geos36 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.6.6 --build-arg BUILD_TOOL=autotools -t isciences/exactextract-test-env:geos36 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos36
 
-docker build --pull --build-arg GEOS_VERSION=3.7.3 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos37 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.7.5 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos37 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos37
 
-docker build --pull --build-arg GEOS_VERSION=3.8.2 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos38 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.8.3 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos38 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos38
 
-docker build --pull --build-arg GEOS_VERSION=3.9.1 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos39 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.9.4 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos39 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos39
 
-docker build --pull --build-arg GEOS_VERSION=3.10.0 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos310 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.10.5 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos310 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos310
+
+docker build --pull --build-arg GEOS_VERSION=3.11.2 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos311 - < Dockerfile.unittest
+docker push isciences/exactextract-test-env:geos311
+
+docker build --pull --build-arg GEOS_VERSION=3.12.0 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos312 - < Dockerfile.unittest
+docker push isciences/exactextract-test-env:geos312
 
 # Build base image for exactextract build
 docker build --pull -t isciences/exactextract-build-env:latest - < Dockerfile.gitlabci
