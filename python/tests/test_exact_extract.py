ffc4189cb82c5e42199e1e585c95f111025287e1
Daniel Baston <dbaston@gmail.com>
2023-12-02 20:49:51 -0500

    Python: support weights arg to exact_extract

--- python/tests/test_exact_extract.py
+++ python/tests/test_exact_extract.py
@@ -37,11 +37,11 @@ def make_rect(xmin, ymin, xmax, ymax, fid=1):
         ("variety", 9),
         ("variance", 5),
         ("stdev", math.sqrt(5)),
-        ("coefficient_of_variation", math.sqrt(5)/5)
+        ("coefficient_of_variation", math.sqrt(5) / 5),
     ],
 )
-def test_exact_extract_stats(stat, expected):
-    rast = make_square_raster(3)
+def test_basic_stats(stat, expected):
+    rast = NumPyRasterSource(np.arange(1, 10).reshape(3, 3))
     square = JSONFeatureSource(make_rect(0.5, 0.5, 2.5, 2.5), "id")
 
     assert exact_extract(rast, square, stat)[0]["properties"][stat] == pytest.approx(
@@ -49,8 +49,8 @@ def test_exact_extract_stats(stat, expected):
     )
 
 
-def test_exact_extract_multiple_stats():
-    rast = make_square_raster(3)
+def test_multiple_stats():
+    rast = NumPyRasterSource(np.arange(1, 10).reshape(3, 3))
     square = JSONFeatureSource(make_rect(0.5, 0.5, 2.5, 2.5), "id")
 
     assert exact_extract(rast, square, ("min", "max", "mean"))[0]["properties"] == {
@@ -58,3 +58,88 @@ def test_exact_extract_multiple_stats():
         "max": 9,
         "mean": 5,
     }
+
+
+@pytest.mark.parametrize("stat", ("mean", "sum", "stdev", "variance"))
+def test_weighted_stats_equal_weights(stat):
+    rast = NumPyRasterSource(np.arange(1, 10).reshape(3, 3))
+    weights = NumPyRasterSource(np.full((3, 3), 1))
+    square = JSONFeatureSource(make_rect(0.5, 0.5, 2.5, 2.5), "id")
+
+    weighted_stat = f"weighted_{stat}"
+
+    results = exact_extract(rast, square, [stat, weighted_stat], weights=weights)[0][
+        "properties"
+    ]
+
+    assert results[stat] == results[weighted_stat]
+
+
+@pytest.mark.parametrize(
+    "stat,expected",
+    [
+        ("weighted_mean", (0.25 * 7 + 0.5 * 8 + 0.25 * 9) / (0.25 + 0.5 + 0.25)),
+        ("weighted_sum", (0.25 * 7 + 0.5 * 8 + 0.25 * 9)),
+        ("weighted_stdev", 0.7071068),
+        ("weighted_variance", 0.5),
+    ],
+)
+def test_weighted_stats_unequal_weights(stat, expected):
+    rast = NumPyRasterSource(np.arange(1, 10).reshape(3, 3))
+    weights = NumPyRasterSource(np.array([[0, 0, 0], [0, 0, 0], [1, 1, 1]]))
+    square = JSONFeatureSource(make_rect(0.5, 0.5, 2.5, 2.5), "id")
+
+    assert exact_extract(rast, square, stat, weights=weights)[0]["properties"][
+        stat
+    ] == pytest.approx(expected)
+
+
+def test_frac():
+    rast = NumPyRasterSource(np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]]))
+    squares = JSONFeatureSource(
+        [make_rect(0.5, 0.5, 1.0, 1.0, "a"), make_rect(0.5, 0.5, 2.5, 2.5, "b")], "id"
+    )
+
+    results = exact_extract(rast, squares, ["count", "frac"])
+
+    assert results[0]["properties"] == {
+        "count": 0.25,
+        # "frac_1" : 0,
+        # "frac_2" : 0,
+        "frac_3": 1.00,
+    }
+
+    assert results[1]["properties"] == {
+        "count": 4,
+        "frac_1": 0.25,
+        "frac_2": 0.5,
+        "frac_3": 0.25,
+    }
+
+    pytest.xfail("missing placeholder results for frac_1 and frac_2")
+
+
+def test_weighted_frac():
+    rast = NumPyRasterSource(np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]]))
+    weights = NumPyRasterSource(np.array([[3, 3, 3], [2, 2, 2], [1, 1, 1]]))
+    squares = JSONFeatureSource(
+        [make_rect(0.5, 0.5, 1.0, 1.0, "a"), make_rect(0.5, 0.5, 2.5, 2.5, "b")], "id"
+    )
+
+    results = exact_extract(rast, squares, ["weighted_frac", "sum"], weights=weights)
+
+    assert results[0]["properties"] == {
+        # "weighted_frac_1" : 0,
+        # "weighted_frac_2" : 0,
+        "weighted_frac_3": 1,
+        "sum": 0.75,
+    }
+
+    assert results[1]["properties"] == {
+        "weighted_frac_1": 0.375,
+        "weighted_frac_2": 0.5,
+        "weighted_frac_3": 0.125,
+        "sum": 8,
+    }
+
+    pytest.xfail("missing placeholder results for wehgted_frac_1 and weighted_frac_2")
