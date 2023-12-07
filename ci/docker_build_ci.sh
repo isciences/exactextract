b1494679ddc8816e4c0119db49bd38f9eb824485
Daniel Baston <dbaston@gmail.com>
2023-12-07 09:11:20 -0500

    CI: Update GEOS versions in test containers

--- ci/docker_build_ci.sh
+++ ci/docker_build_ci.sh
@@ -11,17 +11,17 @@ docker push isciences/exactextract-test-env:geos36
 docker build --pull --build-arg GEOS_VERSION=3.7.5 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos37 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos37
 
-docker build --pull --build-arg GEOS_VERSION=3.8.3 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos38 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.8.4 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos38 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos38
 
-docker build --pull --build-arg GEOS_VERSION=3.9.4 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos39 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.9.5 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos39 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos39
 
-docker build --pull --build-arg GEOS_VERSION=3.10.5 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos310 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.10.6 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos310 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos310
 
-docker build --pull --build-arg GEOS_VERSION=3.11.2 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos311 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.11.3 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos311 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos311
 
-docker build --pull --build-arg GEOS_VERSION=3.12.0 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos312 - < Dockerfile.unittest
+docker build --pull --build-arg GEOS_VERSION=3.12.1 --build-arg BUILD_TOOL=cmake -t isciences/exactextract-test-env:geos312 - < Dockerfile.unittest
 docker push isciences/exactextract-test-env:geos312
