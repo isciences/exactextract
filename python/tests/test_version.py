a63721bdd61b1d24fd9711339dce39f1c2412699
Daniel Baston <dbaston@gmail.com>
2024-03-25 13:58:18 -0400

    Python: ensure __version__ matches package metadata

--- python/tests/test_version.py
+++ python/tests/test_version.py
@@ -1,8 +1,24 @@
 import re
 
+import pytest
+
 import exactextract
 
 
-def test_version():
+def test_version_format():
 
     assert re.match("[0-9]+[.][0-9]+[.][0-9]+", exactextract.__version__)
+
+
+def test_version_consistency():
+
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        mdversion = version("exactextract")
+        if mdversion.endswith("dev0"):
+            pytest.skip()
+    except PackageNotFoundError:
+        pytest.skip()
+
+    assert mdversion == exactextract.__version__
