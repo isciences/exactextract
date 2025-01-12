391177a9185ac81b36180c9705da2e583c74817b
Daniel Burke <danieljburke@me.com>
2025-01-12 21:42:02 +1100

    Refactor to support multiple operations.

--- src/raster_parallel_processor.cpp
+++ src/raster_parallel_processor.cpp
@@ -53,10 +53,10 @@ typedef std::shared_ptr<StatsRegistry> StatsRegistryPtr;
 
 struct ZonalStatsCalc
 {
-    Grid<bounded_extent> subgrid;
-    FeatureHits hits;
-    RasterSource* source;
-    std::unique_ptr<RasterVariant> values;
+    RasterSource* source = nullptr;
+    Grid<bounded_extent> subgrid = Grid<bounded_extent>::make_empty();
+    FeatureHits hits = std::vector<const Feature*>();
+    std::unique_ptr<RasterVariant> values = nullptr;
 };
 
 void
@@ -86,16 +86,24 @@ RasterParallelProcessor::process()
     read_features();
     populate_index();
 
-    std::set<RasterSource*> rasterSources;
+    std::set<RasterSource*> raster_sources;
     for (const auto& op : m_operations) {
-        rasterSources.insert(op->values);
+        raster_sources.insert(op->values);
         m_reg.prepare(*op);
     }
 
-    auto grid = common_grid(m_operations.begin(), m_operations.end(), m_grid_compat_tol);
-    auto subgrids = subdivide(grid, m_max_cells_in_memory);
-    const auto totalSubgrids = subgrids.size();
-    size_t processedSubgrids = 0;
+    std::vector<ZonalStatsCalc> raster_grids;
+    for (auto& source : raster_sources) {
+        auto subgrids = subdivide(source->grid(), m_max_cells_in_memory);
+        for (auto& subgrid : subgrids) {
+            raster_grids.push_back({
+                .source = source,
+                .subgrid = subgrid
+            });
+        }
+    }
+    const auto total_subgrids = raster_grids.size();
+    size_t processed_subgrids = 0;
 
     oneapi::tbb::enumerable_thread_specific<GEOSContextHandle_t> geos_context([]() -> GEOSContextHandle_t {
         return initGEOS_r(errorHandlerParallel, errorHandlerParallel);
@@ -103,23 +111,23 @@ RasterParallelProcessor::process()
 
     // clang-format off
     oneapi::tbb::parallel_pipeline(m_tokens, 
-        oneapi::tbb::make_filter<void, Grid<bounded_extent>>(oneapi::tbb::filter_mode::serial_in_order,
-        [&subgrids] (oneapi::tbb::flow_control& fc) -> Grid<bounded_extent> {
+        oneapi::tbb::make_filter<void, ZonalStatsCalc>(oneapi::tbb::filter_mode::serial_in_order,
+        [&raster_grids] (oneapi::tbb::flow_control& fc) -> ZonalStatsCalc {
             //TODO: split subgridding by raster to optimise IO based on raster block size per input?
             //logic below currently assumes that all raster inputs have the same total geospatial coverage
-            if (subgrids.empty()) {
+            if (raster_grids.empty()) {
                 fc.stop();
-                return Grid<bounded_extent>::make_empty();
+                return {nullptr, Grid<bounded_extent>::make_empty()};
             }
 
-            auto sg = subgrids.back();
-            subgrids.pop_back();
+            auto sg = std::move(raster_grids.back());
+            raster_grids.pop_back();
             return sg;
         }) &
-        oneapi::tbb::make_filter<Grid<bounded_extent>, std::tuple<Grid<bounded_extent>, FeatureHits>>(oneapi::tbb::filter_mode::parallel,
-        [&geos_context, this] (Grid<bounded_extent> subgrid) -> std::tuple<Grid<bounded_extent>, FeatureHits> {
+        oneapi::tbb::make_filter<ZonalStatsCalc, ZonalStatsCalc>(oneapi::tbb::filter_mode::parallel,
+        [&geos_context, this] (ZonalStatsCalc context) -> ZonalStatsCalc {
             std::vector<const Feature*> hits;
-            auto query_rect = geos_make_box_polygon(geos_context.local(), subgrid.extent());
+            auto query_rect = geos_make_box_polygon(geos_context.local(), context.subgrid.extent());
 
             GEOSSTRtree_query_r(
             geos_context.local(), m_feature_tree.get(), query_rect.get(), [](void* hit, void* userdata) {
@@ -131,48 +139,43 @@ RasterParallelProcessor::process()
             &hits);
 
             if (hits.empty()) {
-                return {subgrid, FeatureHits()};
+                return context;
             }
 
-            return {subgrid, hits};
+            context.hits = hits;
+            return context;
         }) &
-        oneapi::tbb::make_filter<std::tuple<Grid<bounded_extent>, FeatureHits>, ZonalStatsCalc>(oneapi::tbb::filter_mode::serial_out_of_order,
-        [rasterSources, this] (std::tuple<Grid<bounded_extent>, FeatureHits> inputs) -> ZonalStatsCalc {
-            auto& [subgrid, hits] = inputs;
-
-            if (subgrid.empty() || hits.empty()) {
-                return {subgrid, hits, nullptr, nullptr};
+        oneapi::tbb::make_filter<ZonalStatsCalc, ZonalStatsCalc>(oneapi::tbb::filter_mode::serial_out_of_order,
+        [raster_sources, this] (ZonalStatsCalc context) -> ZonalStatsCalc {
+            if (context.subgrid.empty() || context.hits.empty()) {
+                return context;
             }
 
-            if (rasterSources.size() > 1) {
-                    throw std::runtime_error("More than 1 raster sources not yet supported.");
-            }
-            auto raster = *rasterSources.begin();
-            auto values = std::make_unique<RasterVariant>(raster->read_box(subgrid.extent().intersection(raster->grid().extent())));
-
-            return {subgrid, hits, raster, std::move(values)};
+            auto values = std::make_unique<RasterVariant>(context.source->read_box(context.subgrid.extent().intersection(context.source->grid().extent())));
+            context.values = std::move(values);
+            return context;
         }) &
         oneapi::tbb::make_filter<ZonalStatsCalc, std::tuple<Grid<bounded_extent>, StatsRegistryPtr>>(oneapi::tbb::filter_mode::parallel,
-        [&geos_context, this] (ZonalStatsCalc inputs) -> std::tuple<Grid<bounded_extent>, StatsRegistryPtr> {
-            if (inputs.subgrid.empty() || inputs.hits.empty() || !inputs.source || !inputs.values) {
-                return {inputs.subgrid, nullptr};
+        [&geos_context, this] (ZonalStatsCalc context) -> std::tuple<Grid<bounded_extent>, StatsRegistryPtr> {
+            if (context.subgrid.empty() || context.hits.empty() || !context.source || !context.values) {
+                return {context.subgrid, nullptr};
             }
 
             auto block_registry = std::make_shared<StatsRegistry>();
+            std::vector<Operation*> trimmed_ops;
 
             for (const auto& op : m_operations) {
-                block_registry->prepare(*op);
+                if (op->values == context.source) {
+                    block_registry->prepare(*op);
+                    trimmed_ops.push_back(op.get());
+                }
             }
 
-            for (const Feature* f : inputs.hits) {
-                auto coverage = std::make_unique<Raster<float>>(raster_cell_intersection(inputs.subgrid, geos_context.local(), f->geometry()));
+            for (const Feature* f : context.hits) {
+                auto coverage = std::make_unique<Raster<float>>(raster_cell_intersection(context.subgrid, geos_context.local(), f->geometry()));
                 std::set<std::string> processed;
 
-                for (const auto& op : m_operations) {
-                    if (op->values != inputs.source) {
-                        continue;
-                    }
-
+                for (const auto op : trimmed_ops) {
                     //TODO: push this earlier and remove duplicate operations entirely
                     if (processed.find(op->key()) != processed.end()) {
                         continue;
@@ -180,40 +183,36 @@ RasterParallelProcessor::process()
                         processed.insert(op->key());
                     }
 
-                    if (!op->values->grid().extent().contains(inputs.subgrid.extent())) {
-                        continue;
-                    }
-
-                    if (op->weighted() && !op->weights->grid().extent().contains(inputs.subgrid.extent())) {
+                    if (op->weighted() && !op->weights->grid().extent().contains(context.subgrid.extent())) {
                         continue;
                     }
 
                     if (op->weighted()) {
                         //TODO: cache weighted input
-                        auto weights = std::make_unique<RasterVariant>(op->weights->read_box(inputs.subgrid.extent().intersection(op->weights->grid().extent())));
-                        block_registry->update_stats(*f, *op, *coverage, *inputs.values, *weights);
+                        auto weights = std::make_unique<RasterVariant>(op->weights->read_box(context.subgrid.extent().intersection(op->weights->grid().extent())));
+                        block_registry->update_stats(*f, *op, *coverage, *context.values, *weights);
                     } else {
-                        block_registry->update_stats(*f, *op, *coverage, *inputs.values);
+                        block_registry->update_stats(*f, *op, *coverage, *context.values);
                     }
                 }
             }
 
-            return {inputs.subgrid, block_registry};
+            return {context.subgrid, block_registry};
         }) &
         oneapi::tbb::make_filter<std::tuple<Grid<bounded_extent>, StatsRegistryPtr>, void>(oneapi::tbb::filter_mode::serial_out_of_order, 
-        [&processedSubgrids, totalSubgrids, this] (std::tuple<Grid<bounded_extent>, StatsRegistryPtr> registry) {
+        [&processed_subgrids, total_subgrids, this] (std::tuple<Grid<bounded_extent>, StatsRegistryPtr> registry) {
             auto& [registryGrid, regptr] = registry;
             if (regptr) {
                 this->m_reg.merge(*regptr);
             }
 
             if (m_show_progress && !registryGrid.empty()) {
-                processedSubgrids++;
+                processed_subgrids++;
 
                 std::stringstream ss;
                 ss << registryGrid.extent();
 
-                progress(static_cast<double>(processedSubgrids) / static_cast<double>(totalSubgrids), ss.str());
+                progress(static_cast<double>(processed_subgrids) / static_cast<double>(total_subgrids), ss.str());
             }
         })
     );
