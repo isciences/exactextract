4b9513d45ebd1d0807718c105d65c76c1f6c2af7
Pro <pronit@tardis.digital>
2026-01-19 22:30:28 +0000

    feat(python): Add RasterParallelProcessor wrapper class
    - Import _RasterParallelProcessor from native module
    - Create RasterParallelProcessor Python wrapper with threads parameter (default=4)
    - Export in __all__ list
    
    Generated-by: sisyphus

--- python/src/exactextract/processor.py
+++ python/src/exactextract/processor.py
@@ -2,12 +2,13 @@ from typing import List, Optional
 
 from ._exactextract import FeatureSequentialProcessor as _FeatureSequentialProcessor
 from ._exactextract import Processor  # noqa: F401
+from ._exactextract import RasterParallelProcessor as _RasterParallelProcessor
 from ._exactextract import RasterSequentialProcessor as _RasterSequentialProcessor
 from .feature import FeatureSource
 from .operation import Operation
 from .writer import Writer
 
-__all__ = ["FeatureSequentialProcessor", "RasterSequentialProcessor"]
+__all__ = ["FeatureSequentialProcessor", "RasterParallelProcessor", "RasterSequentialProcessor"]
 
 
 class FeatureSequentialProcessor(_FeatureSequentialProcessor):
@@ -51,9 +52,36 @@ class RasterSequentialProcessor(_RasterSequentialProcessor):
             writer (Writer): Writer to use
             op_list (List[Operation]): List of operations
             include_cols: List of columns to copy from
-               input features
+                input features
         """
         super().__init__(ds, writer)
+        for col in include_cols or []:
+            self.add_col(col)
+        for op in op_list:
+            self.add_operation(op)
+
+
+class RasterParallelProcessor(_RasterParallelProcessor):
+    """Binding class around exactextract RasterParallelProcessor"""
+
+    def __init__(
+        self,
+        ds: FeatureSource,
+        writer: Writer,
+        op_list: List[Operation],
+        include_cols: Optional[List[Operation]] = None,
+        threads: int = 4,
+    ):
+        """
+        Args:
+            ds (FeatureSource): Dataset to use
+            writer (Writer): Writer to use
+            op_list (List[Operation]): List of operations
+            include_cols: List of columns to copy from
+                input features
+            threads (int): Number of threads for parallel processing
+        """
+        super().__init__(ds, writer, threads)
         for col in include_cols or []:
             self.add_col(col)
         for op in op_list:
